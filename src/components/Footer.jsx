c7fc280671fabfdd0d3cc553625de101f43072ce
loopinbd <loopinbd@gmail.com>
2025-06-28 01:45:04 +0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <div className="footer-left">© All Rights Reserved 2025</div>
+      <div className="footer-right">Powered by Blockchain</div>
+    </footer>
+  );
+};
+
+export default Footer;
