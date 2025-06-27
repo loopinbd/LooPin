cd22957fc407788ebdb7bcdcd05b81245b923049
loopinbd <loopinbd@gmail.com>
2025-06-28 01:28:47 +0600

    Create BalanceCard.jsx

---
+++ src/components/BalanceCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/BalanceCard.css";
+
+const BalanceCard = ({ balance = 0 }) => {
+  return (
+    <div className="balance-card">
+      <h3 className="balance-title">Current Balance</h3>
+      <div className="balance-amount">${balance.toFixed(2)}</div>
+    </div>
+  );
+};
+
+export default BalanceCard;
