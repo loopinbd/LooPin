ff6f2395170554a49dae783201bbde7bbd431461
loopinbd <loopinbd@gmail.com>
2025-06-28 13:27:56 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css"; // যদি থাকে
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
