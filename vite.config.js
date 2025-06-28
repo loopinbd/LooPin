c6e12e45d5be9f65156ae0ef124e2d1361b6b0f1
loopinbd <loopinbd@gmail.com>
2025-06-28 13:26:48 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // important for Vercel static
+  server: {
+    port: 3000,
+  },
 });
