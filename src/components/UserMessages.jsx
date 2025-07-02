43a56633942a0434cf94344941da3e6cb76b2b19
loopinbd <loopinbd@gmail.com>
2025-07-03 00:51:11 +0600

    Create UserMessages.jsx

---
+++ src/components/UserMessages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserMessages = () => {
+  return (
+    <div className="data-box">
+      <p>No user messages yet.</p>
+    </div>
+  );
+};
+
+export default UserMessages;
