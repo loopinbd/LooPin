c18815b4e477b9293858ad8db3680a23c27eddc1
loopinbd <loopinbd@gmail.com>
2025-07-03 01:00:47 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext"; // ✅ import the provider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider> {/* ✅ provide context */}
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
