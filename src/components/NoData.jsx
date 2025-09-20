2ad873a7b0c1439731bc8b5376b46f75c2766f24
loopinbd <loopinbd@gmail.com>
2025-09-20 19:21:44 +0600

    Delete src directory

--- src/components/NoData.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/NoData.css";
-
-const NoData = ({ message = "No data available." }) => {
-  return <div className="no-data">{message}</div>;
-};
-
-export default NoData;
